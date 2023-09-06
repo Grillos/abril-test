3373da124782f06cca6e38884605bd2b82eed091
Grillos <robsongrillo13@gmail.com>
2023-09-06 20:39:26 -0300

    gerando pdf usando biblioteca itext

--- src/main/java/com/abril/test/controller/SignatureController.java
+++ src/main/java/com/abril/test/controller/SignatureController.java
@@ -1,12 +1,18 @@
 package com.abril.test.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.abril.test.domain.Signature;
@@ -28,6 +34,21 @@ public class SignatureController {
     public ResponseEntity<List<Signature>> find(SignatureRequest request) {
     	List<Signature> signatures = service.find(request);
     	return (signatures != null) ? new ResponseEntity<>(signatures, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+	
+	@RequestMapping(value = "/download", method = RequestMethod.GET)
+    public HttpEntity<byte[]> download() throws IOException {
+
+    	service.generatePdf();
+        byte[] arquivo = Files.readAllBytes( Paths.get("/tmp/EQUIPEDELIMPEZA.pdf") );
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        httpHeaders.add("Content-Disposition", "attachment;filename=\"meu-arquivo.pdf\"");
+
+        HttpEntity<byte[]> entity = new HttpEntity<byte[]>( arquivo, httpHeaders);
+
+        return entity;
     }
     
     
--- src/main/java/com/abril/test/service/SignatureService.java
+++ src/main/java/com/abril/test/service/SignatureService.java
@@ -1,5 +1,8 @@
 package com.abril.test.service;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +15,16 @@ import com.abril.test.exception.ErrorResponseException;
 import com.abril.test.exception.Response;
 import com.abril.test.repository.SignatureRepository;
 import com.abril.test.request.SignatureRequest;
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
 
 @Service
 public class SignatureService {
+	
+	private static final String JPG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLec6j_BxT0wk8wuVC-uBGanFUNe4oz98B0DcYO5ZWdHr1KUo7kZQ2U2SFZqpSFzv7CJQ&usqp=CAU";
 	
 	@Autowired
 	private SignatureRepository repository;
@@ -27,4 +37,29 @@ public class SignatureService {
 				.message(ExceptionEnum.NOT_FOUND.getDescription()).build(),
 		HttpStatus.NOT_FOUND));
 	}
+	
+	public void generatePdf() {
+		try {
+			generatePDFFromImage(JPG, "jpg");
+			
+		} catch (IOException | DocumentException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	private static void generatePDFFromImage(String filename, String extension)
+			throws IOException, BadElementException, DocumentException {
+		Document document = new Document();
+		String input = filename + "." + extension;
+		String output = "/tmp/" + extension + ".pdf";
+		FileOutputStream fos = new FileOutputStream(output);
+		PdfWriter writer = PdfWriter.getInstance(document, fos);
+		writer.open();
+		document.open();
+		document.add(Image.getInstance((new URL(input))));
+		document.close();
+		writer.close();
+	}
+		
 }
