f344c6c9129ac3e2707bb35a1082f1774bc5d653
Grillos <robsongrillo13@gmail.com>
2023-09-08 16:38:24 -0300

    criando metodo para gerar pdf usando multiplas imagens

--- src/main/java/com/abril/test/controller/SignatureController.java
+++ src/main/java/com/abril/test/controller/SignatureController.java
@@ -1,14 +1,18 @@
 package com.abril.test.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,20 +39,21 @@ public class SignatureController {
     	List<Signature> signatures = service.find(request);
     	return (signatures != null) ? new ResponseEntity<>(signatures, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-	
-	@RequestMapping(value = "/download", method = RequestMethod.GET)
-    public HttpEntity<byte[]> download() throws IOException {
+    
+    @RequestMapping(path = "/download", method = RequestMethod.GET)
+    public ResponseEntity<Resource> download(String param) throws IOException {
+    	HttpHeaders headers = new HttpHeaders();
+        
+        service.generatePdf("teste_img_in_pdf_abril");
+    	File file = new File("/tmp/teste_img_in_pdf_abril.pdf");
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
 
-    	service.generatePdf();
-        byte[] arquivo = Files.readAllBytes( Paths.get("/tmp/EQUIPEDELIMPEZA.pdf") );
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-
-        httpHeaders.add("Content-Disposition", "attachment;filename=\"meu-arquivo.pdf\"");
-
-        HttpEntity<byte[]> entity = new HttpEntity<byte[]>( arquivo, httpHeaders);
-
-        return entity;
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
     
     
--- src/main/java/com/abril/test/service/SignatureService.java
+++ src/main/java/com/abril/test/service/SignatureService.java
@@ -2,7 +2,6 @@ package com.abril.test.service;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +17,17 @@ import com.abril.test.request.SignatureRequest;
 import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.PdfWriter;
 
 @Service
 public class SignatureService {
 	
-	private static final String JPG = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLec6j_BxT0wk8wuVC-uBGanFUNe4oz98B0DcYO5ZWdHr1KUo7kZQ2U2SFZqpSFzv7CJQ&usqp=CAU";
-	
+	private static final String JPG_1 = "/home/robson/Imagens/abril01.png";
+	private static final String JPG_2 = "/home/robson/Imagens/abril02.png";
+	private static final String JPG_3 = "/home/robson/Imagens/abril03.png";
+
 	@Autowired
 	private SignatureRepository repository;
 
@@ -38,9 +40,9 @@ public class SignatureService {
 		HttpStatus.NOT_FOUND));
 	}
 	
-	public void generatePdf() {
+	public void generatePdf(String fileName) {
 		try {
-			generatePDFFromImage(JPG, "jpg");
+			generatePDFFromImage(fileName);
 			
 		} catch (IOException | DocumentException e) {
 			e.printStackTrace();
@@ -48,16 +50,27 @@ public class SignatureService {
 	}
 	
 
-	private static void generatePDFFromImage(String filename, String extension)
+	private static void generatePDFFromImage(String filename)
 			throws IOException, BadElementException, DocumentException {
 		Document document = new Document();
-		String input = filename + "." + extension;
-		String output = "/tmp/" + extension + ".pdf";
+		String output = "/tmp/" + filename + ".pdf";
 		FileOutputStream fos = new FileOutputStream(output);
 		PdfWriter writer = PdfWriter.getInstance(document, fos);
 		writer.open();
-		document.open();
-		document.add(Image.getInstance((new URL(input))));
+		document.open();		
+		
+		Image img1 = Image.getInstance(JPG_1);
+		img1.setAlignment(Element.ALIGN_CENTER);
+		
+		Image img2 = Image.getInstance(JPG_2);
+		img2.setAlignment(Element.ALIGN_CENTER);
+		
+		Image img3 = Image.getInstance(JPG_3);
+		img3.setAlignment(Element.ALIGN_CENTER);
+		
+		document.add(img1);
+		document.add(img2);
+		document.add(img3);
 		document.close();
 		writer.close();
 	}
